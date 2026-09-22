ac5ab3f98feb72d47ae2d1ed9e8d1febc1d0e431
yeganehbarati <yeganeh.barati@abn-cleanroomtechnology.com>
2026-09-22 13:46:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'abn-subcontractor-directory-v1';
+const CACHE_NAME = 'abn-subcontractor-directory-v2';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
